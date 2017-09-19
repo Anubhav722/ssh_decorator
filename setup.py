e36d87569afe12f6ce18171d489635af5a4629ff
Uri Goren <urig@yahoo-inc.com>
2017-09-19 15:55:43 +0300

    Adding minimum python package requirement

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
   name = 'ssh_decorator',
   packages = ['ssh_decorator'],
   version = '0.1',
+  python_requires='>=3',
   description = 'Run python code via ssh with a decorator',
   author = 'Uri Goren',
   author_email = 'uri@goren4u.com',
