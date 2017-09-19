60026141dbe2bb2f0411130df996b1e031b28167
Uri Goren <urig@yahoo-inc.com>
2017-09-19 15:38:58 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ssh_decorator',
+  packages = ['ssh_decorator'],
+  version = '0.1',
+  description = 'Run python code via ssh with a decorator',
+  author = 'Uri Goren',
+  author_email = 'uri@goren4u.com',
+  url = 'https://github.com/urigoren/ssh_decorator',
+  download_url = 'https://github.com/urigoren/ssh_decorator/archive/0.1.tar.gz',
+  keywords = ['decorator', 'jupyter', 'ssh'],
+  classifiers = [],
+)
