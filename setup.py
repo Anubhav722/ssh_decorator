dd41b3670d34f80fc9a8f73074fd24bacaa6c7c0
Uri Goren <uri@goren4u.com>
2018-01-18 22:37:40 +0200

    Bug fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
           'pandas',
           'paramiko',
   ],
-  version = '0.22',
+  version = '0.23',
   python_requires='>=2.7',
   description = 'Run python code via ssh with a decorator',
   author = 'Uri Goren',
--- ssh_decorate/__init__.py
+++ ssh_decorate/__init__.py
@@ -1,1 +1,1 @@
-from ssh_connect import ssh_connect
+from .ssh_connect import ssh_connect
